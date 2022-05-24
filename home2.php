e53a8d6a0e8f781aa06ef91f767cf332133268b7
Uênio Oliveira <ueniosa@gmail.com>
2022-05-23 21:43:17 -0300

    Teste do Formulário

--- home2.php
+++ home2.php
@@ -16,6 +16,15 @@
                         <input	type="submit" value="Pesquisar"/>
                     </fieldset>
                 </form>
+                <?php
+                    $entradadoUsuario = $_GET;
+                    if (isset($entradadoUsuario)){
+                        echo "Exibindo resultados para: " . $entradadoUsuario;
+                    }
+                    else {
+                        echo "Por favor, digite algo no campo de pesquisa";
+                    }
+                ?>
             </div>
  <!--           <div class="selectionfilters">Filtros<input type="search"></div>   -->
         </section>
